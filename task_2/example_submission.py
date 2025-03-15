00e2b1d09c66da2258f8659b7ce8b23817e3bed4
Piotr Kubala <pkubala@student.agh.edu.pl>
2025-03-15 15:37:01 +0100

    task_3 v1

--- task_2/example_submission.py
+++ task_2/example_submission.py
@@ -11,17 +11,22 @@ import torch.nn as nn
 from torch.utils.data import Dataset
 from typing import Tuple
 
+import os
+from dotenv import load_dotenv
 
-TOKEN = ...                         # Your token here
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
 SUBMIT_URL = "149.156.182.9:6060/task-2/submit"
 RESET_URL = "149.156.182.9:6060/task-2/reset"
 QUERY_URL = "149.156.182.9:6060/task-2/query"
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+DATASET_PATH = os.getenv("TASK_2_DATA_PUBLIC_PATH")
 
 class TaskDataset(Dataset):
     def __init__(self, transform=None):
-
         self.ids = []
         self.imgs = []
         self.labels = []
@@ -40,9 +45,8 @@ class TaskDataset(Dataset):
         return len(self.ids)
 
 
-
 def quering_example():
-    dataset = torch.load(...)                   # Path to ModelStealingPub.pt
+    dataset = torch.load(DATASET_PATH)                   # Path to ModelStealingPub.pt
     images = [dataset.imgs[idx] for idx in np.random.permutation(1000)]
 
     image_data = []
@@ -110,4 +114,3 @@ def submitting_example():
 if __name__ == '__main__':
     quering_example()
     submitting_example()
-
